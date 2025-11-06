b41ef83cd23affe71e7eaafb6b8c0fdd77e974c4
Lonjezo-dev <samuelgondwe741@gmail.com>
2025-11-06 23:50:47 +0200

    create a inventory model

---
+++ model/Inventory.js
@@ -0,0 +1,45 @@
+const sequelize = require("../config/database");
+const { DataTypes } = require("sequelize");
+
+const Inventory = sequelize.define("Inventory", {
+   // Each attribute will pair with a column
+      // Here we define our model attributes
+
+      // Our primaryKey, inventory id, our unique identifier
+   id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,},       
+    item_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    quantity: { 
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    unit: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    last_updated: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+}, 
+// Model options        
+
+{
+    // Specify the table name
+   tableName: "inventory",
+});
+
+module.exports = {
+    Inventory,
+    sequelize
+};
+
+
+
+
